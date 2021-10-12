e66b9c6bd9e18e61e4349212b41564a17a70672b
move <move@zealand.dk>
2021-10-12 10:37:48 +0200

    added item class

---
+++ Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ItemLibrary
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ItemQuality { get; set; }
+        public int Quantity { get; set; }
+
+        public Item(int Id, string Name, int ItemQuality, int Quantity)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.ItemQuality = ItemQuality;
+            this.Quantity = Quantity;
+        }
+
+        public Item()
+        {
+        }
+    }
+}
