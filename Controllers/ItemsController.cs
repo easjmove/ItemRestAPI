93aaf02a01423870876320777f13c877d9c8428d
move <move@zealand.dk>
2021-10-12 09:48:20 +0200

    Add project files.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ItemLibrary;
+using ItemRestAPI.Managers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ItemRestAPI.Controllers
+{
+    [Route("Items")]
+    [EnableCors("AllowOnlyGet")]
+    [ApiExplorerSettings(GroupName ="v2")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly ItemsManager _manager = new ItemsManager();
+
+        // GET: /Items?contains=wat
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Item>> Get([FromQuery] string contains)
+        {
+            IEnumerable<Item> items = _manager.GetAll(contains);
+            if (items.Count() == 0)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok(items);
+            }
+        }
+
+        // GET /Items/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<Item> Get(int id)
+        {
+            Item item = _manager.GetById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(item);
+            }
+        }
+
+        // POST /<ItemsController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        
+        [HttpPost]
+        public ActionResult<Item> Post([FromBody] Item item)
+        {
+            return Ok(_manager.Add(item));
+        }
+
+        // PUT /Items/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [DisableCors]
+        [HttpPut("{id}")]
+        public ActionResult<Item> Put(int id, [FromBody] Item item)
+        {
+            Item result = _manager.Update(id, item);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(result);
+            }
+        }
+
+        // DELETE /<ItemsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{id}")]
+        public ActionResult<Item> Delete(int id)
+        {
+            Item result = _manager.Delete(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(result);
+            }
+        }
+    }
+}
---
+++ Managers/ItemsManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ItemLibrary;
+
+namespace ItemRestAPI.Managers
+{
+    public class ItemsManager
+    {
+        private static int _nextID = 1;
+        private static readonly List<Item> Data = new List<Item>
+        {
+            new Item{Id = _nextID++, Name="Soda", ItemQuality = 3, Quantity = 5 },
+            new Item{Id = _nextID++, Name="Water", ItemQuality = 4, Quantity = 3 }
+            };
+
+        public IEnumerable<Item> GetAll(string contains)
+        {
+            List<Item> items = new List<Item>(Data);
+            if (contains != null)
+            {
+                items = items.FindAll(item => item.Name.Contains(contains,StringComparison.OrdinalIgnoreCase));
+            }
+            return items;
+        }
+
+        public Item GetById(int id)
+        {
+            return Data.Find(item => item.Id == id);
+        }
+
+        public Item Add(Item newItem)
+        {
+            newItem.Id = _nextID++;
+            Data.Add(newItem);
+            return newItem;
+        }
+
+        public Item Delete(int id)
+        {
+            Item item = GetById(id);
+            if (item == null) return null;
+            Data.Remove(item);
+            return item;
+        }
+
+        public Item Update(int id, Item updates)
+        {
+            Item item = GetById(id);
+            if (item == null) return null;
+            item.Name = updates.Name;
+            item.ItemQuality = updates.ItemQuality;
+            item.Quantity = updates.Quantity;
+            return item;
+        }
+    }
+}
